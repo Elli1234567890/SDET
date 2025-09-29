3f69e0ad161f9938cc0be6a92ad9fa92a25e09d0
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2025-09-29 19:44:39 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,10 +4,10 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    options = Options
+    options = Options()
     options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     driver.implicitly_wait(3)
     yield driver
-    driver.close()
+    driver.quit()
